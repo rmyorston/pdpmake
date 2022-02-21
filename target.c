rmyorston/pdpmake

commit c5869feda9f4ba483b4cb4fce65d8d23dcd72c04

Add a function to find commands for a target
There are a few places where we need to know if a target has
commands associated with it.  Add getcmd() to do this, it
makes the code tidier.

target.c
@@ -93,92 +93,6 @@ freecmds(struct cmd *cp)
 	}
 }
 
-void
-freerules(struct rule *rp)
-{
-	struct rule *nextrp;
-
-	for (; rp; rp = nextrp) {
-		nextrp = rp->r_next;
-		freedeps(rp->r_dep);
-		freecmds(rp->r_cmd);
-		free(rp);
-	}
-}
-
-static void *
-inc_ref(void *vp)
-{
-	if (vp) {
-		struct depend *dp = vp;
-		if (dp->d_refcnt == INT_MAX)
-			error("out of memory");
-		dp->d_refcnt++;
-	}
-	return vp;
-}
-
-/*
- * Add a new rule to a target.  This checks to see if commands already
- * exist for the target.  If flag is TRUE the target can have multiple
- * rules with commands (double colon rules).
- *
- * i)  If the name is a special target and there are no prerequisites
- *     or commands to be added remove all prerequisites and commands.
- *     This is necessary when clearing a built-in inference rule.
- * ii) If name is a special target and has commands, replace them.
- *     This is for redefining commands for an inference rule.
- */
-void
-addrule(struct name *np, struct depend *dp, struct cmd *cp, int flag)
-{
-	bool hascmds = FALSE;	// Target has commands
-	struct rule *rp;
-	struct rule **rpp;
-
-#if ENABLE_FEATURE_MAKE_EXTENSIONS
-	// Can't mix colon and double colon rules
-	if (!posix && (np->n_flag & N_TARGET)) {
-		if (!(np->n_flag & N_DOUBLE) != !flag)		// like xor
-			error("inconsistent rules for target %s", np->n_name);
-	}
-#endif
-
-	// Clear out prerequisites and commands
-	if ((np->n_flag & N_SPECIAL) && !dp && !cp) {
-		freerules(np->n_rule);
-		np->n_rule = NULL;
-		return;
-	}
-
-	// This loop must run to completion since rpp is used later.
-	rpp = &np->n_rule;
-	for (rp = np->n_rule; rp; rp = rp->r_next) {
-		if (rp->r_cmd)
-			hascmds = TRUE;
-		rpp = &rp->r_next;
-	}
-
-	if (hascmds && cp && !(np->n_flag & N_DOUBLE)) {
-		// Handle the inference rule redefinition case
-		if ((np->n_flag & N_SPECIAL) && !dp) {
-			freerules(np->n_rule);
-			rpp = &np->n_rule;
-		} else {
-			error("commands defined twice for target %s", np->n_name);
-		}
-	}
-
-	*rpp = rp = xmalloc(sizeof(struct rule));
-	rp->r_next = NULL;
-	rp->r_dep = inc_ref(dp);
-	rp->r_cmd = inc_ref(cp);
-
-	np->n_flag |= N_TARGET;
-	if (flag)
-		np->n_flag |= N_DOUBLE;
-}
-
 struct name *namehead;
 struct name *nametail;
 struct name *firstname;
@@ -222,6 +136,20 @@ newname(const char *name)
 	return np;
 }
 
+/*
+ * Return the commands on the first rule that has them or NULL.
+ */
+struct cmd *
+getcmd(struct name *np)
+{
+	struct rule *rp;
+
+	for (rp = np->n_rule; rp; rp = rp->r_next)
+		if (rp->r_cmd)
+			return rp->r_cmd;
+	return NULL;
+}
+
 #if ENABLE_FEATURE_CLEAN_UP
 void
 freenames(void)
@@ -236,3 +164,82 @@ freenames(void)
 	}
 }
 #endif
+
+void
+freerules(struct rule *rp)
+{
+	struct rule *nextrp;
+
+	for (; rp; rp = nextrp) {
+		nextrp = rp->r_next;
+		freedeps(rp->r_dep);
+		freecmds(rp->r_cmd);
+		free(rp);
+	}
+}
+
+static void *
+inc_ref(void *vp)
+{
+	if (vp) {
+		struct depend *dp = vp;
+		if (dp->d_refcnt == INT_MAX)
+			error("out of memory");
+		dp->d_refcnt++;
+	}
+	return vp;
+}
+
+/*
+ * Add a new rule to a target.  This checks to see if commands already
+ * exist for the target.  If flag is TRUE the target can have multiple
+ * rules with commands (double colon rules).
+ *
+ * i)  If the name is a special target and there are no prerequisites
+ *     or commands to be added remove all prerequisites and commands.
+ *     This is necessary when clearing a built-in inference rule.
+ * ii) If name is a special target and has commands, replace them.
+ *     This is for redefining commands for an inference rule.
+ */
+void
+addrule(struct name *np, struct depend *dp, struct cmd *cp, int flag)
+{
+	struct rule *rp;
+	struct rule **rpp;
+
+#if ENABLE_FEATURE_MAKE_EXTENSIONS
+	// Can't mix colon and double colon rules
+	if (!posix && (np->n_flag & N_TARGET)) {
+		if (!(np->n_flag & N_DOUBLE) != !flag)		// like xor
+			error("inconsistent rules for target %s", np->n_name);
+	}
+#endif
+
+	// Clear out prerequisites and commands
+	if ((np->n_flag & N_SPECIAL) && !dp && !cp) {
+		freerules(np->n_rule);
+		np->n_rule = NULL;
+		return;
+	}
+
+	if (cp && !(np->n_flag & N_DOUBLE) && getcmd(np)) {
+		// Handle the inference rule redefinition case
+		if ((np->n_flag & N_SPECIAL) && !dp)
+			freerules(np->n_rule);
+		else
+			error("commands defined twice for target %s", np->n_name);
+	}
+
+	rpp = &np->n_rule;
+	while (*rpp)
+		rpp = &(*rpp)->r_next;
+
+	*rpp = rp = xmalloc(sizeof(struct rule));
+	rp->r_next = NULL;
+	rp->r_dep = inc_ref(dp);
+	rp->r_cmd = inc_ref(cp);
+
+	np->n_flag |= N_TARGET;
+	if (flag)
+		np->n_flag |= N_DOUBLE;
+}

make.c
@@ -141,10 +141,6 @@ make(struct name *np, int level)
 	struct name *impdep = NULL;	// implicit prerequisite
 	struct rule imprule;
 	struct cmd *sc_cmd = NULL;	// commands for single colon rule
-#if ENABLE_FEATURE_MAKE_EXTENSIONS
-	bool dc_infer = FALSE;
-	struct name *locdep = NULL;
-#endif
 	char *newer = NULL;
 	time_t dtime = 1;
 	bool didsomething = 0;
@@ -156,20 +152,10 @@ make(struct name *np, int level)
 	if (!np->n_time)
 		modtime(np);		// Get modtime of this file
 
-	// Check if target requires the use of an inference rule.
-	// For single colon rules this is if *no* rule has commands;
-	// for double colon rules it's if *any* rule has *no* commands.
-	for (rp = np->n_rule; rp; rp = rp->r_next) {
-		if (rp->r_cmd)
-			sc_cmd = rp->r_cmd;
-#if ENABLE_FEATURE_MAKE_EXTENSIONS
-		else
-			dc_infer = TRUE;
-#endif
-	}
-
 	if (!(np->n_flag & N_DOUBLE)) {
-		// Look for an inference rule
+		// Find the commands needed for a single colon rule, using
+		// an inference rule or .DEFAULT rule if necessary
+		sc_cmd = getcmd(np);
 		if (!sc_cmd) {
 			impdep = dyndep(np, &imprule);
 			if (impdep) {
@@ -188,19 +174,27 @@ make(struct name *np, int level)
 		}
 	}
 #if ENABLE_FEATURE_MAKE_EXTENSIONS
-	else if (dc_infer) {
-		impdep = dyndep(np, &imprule);
-		if (!impdep)
-			error("don't know how to make %s", np->n_name);
+	else {
+		// If any double colon rule has no commands we need
+		// an inference rule
+		for (rp = np->n_rule; rp; rp = rp->r_next) {
+			if (!rp->r_cmd) {
+				impdep = dyndep(np, &imprule);
+				if (!impdep)
+					error("don't know how to make %s", np->n_name);
+				break;
+			}
+		}
 	}
 #endif
 
 	for (rp = np->n_rule; rp; rp = rp->r_next) {
 #if ENABLE_FEATURE_MAKE_EXTENSIONS
+		struct name *locdep = NULL;
+
 		// Each double colon rule is handled separately.
 		if ((np->n_flag & N_DOUBLE)) {
 			// If the rule has no commands use the inference rule.
-			locdep = NULL;
 			if (!rp->r_cmd) {
 				locdep = impdep;
 				imprule.r_dep->d_next = rp->r_dep;
@@ -214,7 +208,7 @@ make(struct name *np, int level)
 #endif
 		for (dp = rp->r_dep; dp; dp = dp->d_next) {
 			// Make prerequisite
-			estat |= make(dp->d_name, level+1);
+			estat |= make(dp->d_name, level + 1);
 
 			// Make a string listing prerequisites newer than target
 			// (but not if we were invoked with -q).

make.h
@@ -165,6 +165,7 @@ struct name *dyndep(struct name *np, struct rule *imprule);
 char *getrules(char *s, int size);
 struct name *findname(const char *name);
 struct name *newname(const char *name);
+struct cmd *getcmd(struct name *np);
 void freenames(void);
 struct depend *newdep(struct name *np, struct depend *dp);
 void freedeps(struct depend *dp);

rules.c
@@ -45,9 +45,8 @@ dyndep(struct name *np, struct rule *imprule)
 	struct name *xp;		// Suffixes
 	struct name *sp;		// Suffix rule
 	struct name *pp = NULL;	// Implicit prerequisite
-	struct rule *rp, *rp2;
+	struct rule *rp;
 	struct depend *dp;
-	bool hascmds = FALSE;
 
 	member = NULL;
 	name = splitlib(np->n_name, &member);
@@ -67,16 +66,7 @@ dyndep(struct name *np, struct rule *imprule)
 				pp = namecat(base, newsuff, TRUE);
 				if (!pp->n_time)
 					modtime(pp);
-				if (!pp->n_time) {
-					// File doesn't currently exist, can we make it?
-					for (rp2 = pp->n_rule; rp2; rp2 = rp2->r_next) {
-						if (rp2->r_cmd) {
-							hascmds = TRUE;
-							break;
-						}
-					}
-				}
-				if (pp->n_time || hascmds) {
+				if (pp->n_time || getcmd(np)) {
 					// Prerequisite exists or we know how to make it
 					imprule->r_dep = newdep(pp, NULL);
 					imprule->r_cmd = sp->n_rule->r_cmd;

commit 0541b5a80620d6e050a896e5a68cdb1fbbe04302

Fix an error in inference rule redefinition
The commit c5869fe (Add a function to find commands for a target)
introduced an error in the inference rule redefinition case of
addrule().  The old rules were freed but the pointer to them wasn't
set to NULL.

target.c
@@ -224,10 +224,12 @@ addrule(struct name *np, struct depend *dp, struct cmd *cp, int flag)
 
 	if (cp && !(np->n_flag & N_DOUBLE) && getcmd(np)) {
 		// Handle the inference rule redefinition case
-		if ((np->n_flag & N_SPECIAL) && !dp)
+		if ((np->n_flag & N_SPECIAL) && !dp) {
 			freerules(np->n_rule);
-		else
+			np->n_rule = NULL;
+		} else {
 			error("commands defined twice for target %s", np->n_name);
+		}
 	}
 
 	rpp = &np->n_rule;